8b75014c598f775f94c2516b9d8647e847f3dd81
Ryan Wade <ryan.wade@protonmail.com>
2017-06-01 17:11:57 +0800

    add test

---
+++ src/test/java/com/ireul/nerf/utils/AnnotationUtilsTest.java
@@ -0,0 +1,63 @@
+package com.ireul.nerf.utils;
+
+import org.junit.Test;
+
+import java.lang.annotation.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ryan on 6/1/17.
+ */
+public class AnnotationUtilsTest {
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.METHOD)
+    private @interface TargetAnnotations {
+        TargetAnnotation[] value();
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.METHOD)
+    @Repeatable(TargetAnnotations.class)
+    private @interface TargetAnnotation {
+    }
+
+    private static class TargetClass {
+
+        @TargetAnnotation
+        static void targetStaticMethod() {
+        }
+
+        @TargetAnnotation
+        void targetMethod1() {
+        }
+
+        @TargetAnnotation
+        @TargetAnnotation
+        @TargetAnnotation
+        void targetMethod2() {
+        }
+
+    }
+
+    @Test
+    public void forEachInstanceMethod() throws Exception {
+        final int[] counter = {0};
+        final int[] method1Counter = {0};
+        final int[] method2Counter = {0};
+        AnnotationUtils.forEachInstanceMethod(TargetClass.class, TargetAnnotation.class, (m, a) -> {
+            counter[0]++;
+            if (m.getName().equalsIgnoreCase("targetMethod1")) {
+                method1Counter[0]++;
+            }
+            if (m.getName().equalsIgnoreCase("targetMethod2")) {
+                method2Counter[0]++;
+            }
+        });
+        assertTrue(counter[0] == 4);
+        assertTrue(method1Counter[0] == 1);
+        assertTrue(method2Counter[0] == 3);
+    }
+
+}
---
+++ src/test/java/com/ireul/nerf/web/route/RouteUtilsTest.java
@@ -0,0 +1,51 @@
+package com.ireul.nerf.web.route;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ryan on 6/1/17.
+ */
+public class RouteUtilsTest {
+
+    @Test
+    public void scanPackage() throws Exception {
+        //TODO: add tests
+    }
+
+    @Test
+    public void scanClass() throws Exception {
+        //TODO: add tests
+    }
+
+    @Test
+    public void matchPath() throws Exception {
+        RouteResult result = new RouteResult();
+        // 0. basic match
+        boolean ok1 = RouteUtils.matchPath("/index", "/index", result);
+        assertTrue(ok1);
+        assertFalse(result.hasNamedPaths());
+        // 1. named match
+        result = new RouteResult();
+        boolean ok2 = RouteUtils.matchPath("/index/:name", "/index/hello", result);
+        assertTrue(ok2);
+        assertTrue(result.hasNamedPaths());
+        assertTrue(result.namedPaths().size() == 1);
+        assertEquals(result.namedPaths().get("name"), "hello");
+        // 2. multiple match
+        result = new RouteResult();
+        boolean ok3 = RouteUtils.matchPath("/index/:name/:value", "/index/hello/world", result);
+        assertTrue(ok3);
+        assertTrue(result.hasNamedPaths());
+        assertTrue(result.namedPaths().size() == 2);
+        assertEquals(result.namedPaths().get("name"), "hello");
+        assertEquals(result.namedPaths().get("value"), "world");
+    }
+
+    @Test
+    public void matchMime() throws Exception {
+        //TODO: add tests
+    }
+
+}
